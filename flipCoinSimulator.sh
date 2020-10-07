c79813caf810c7d82a95577e88e314d1cecd66b5
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-07 15:35:46 +0530

    User Case1 done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Simulation code"
 
+##Displaying Heads or Tails UserCase1
+Result=$((RANDOM%2))
+if [ $Result -eq 1 ]
+then
+     echo "Heads"
+else
+     echo "Tails"
+fi
+
