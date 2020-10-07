f937cf3e33049dd98c36e5c50ed5c5c93286937c
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-07 16:05:28 +0530

    User Case2 done

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,3 +10,20 @@ else
      echo "Tails"
 fi
 
+##Showing number of times each results have won. UserCase2
+heads=0;
+tails=0;
+while [[ $heads -le 10 && $tails -le 10 ]]
+do
+	Result=$((RANDOM%2))
+	if [ $Result -eq 1 ]
+	then
+	    echo "Head won"
+	    heads=$(($heads+1))
+	else
+	    echo "Tails won"
+	    tails=$(($tails+1))
+	fi
+done
+echo "Number of times heads won: " $heads
+echo "Number of times tails won: " $tails
